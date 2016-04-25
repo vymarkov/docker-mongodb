6b26d201f1ec35eccc9d5440ac35d655aa90ef42
Vitaly Markov <v.y.markov@gmail.com>
2016-04-26 02:01:53 +0300

    Update build_and_push_image.sh

--- build_and_push_image.sh
+++ build_and_push_image.sh
@@ -2,4 +2,5 @@ TAG=${IMAGE_TAG:-"latest"}
 
 docker build --no-cache -t vymarkov/mongo -f Dockerfile .
 docker tag vymarkov/mongo vymarkov/mongo:$TAG
+docker login --username $DOCKER_REGISTRY_USERNAME --email $DOCKER_REGISTRY_EMAIL --password $DOCKER_REGISTRY_PASS
 docker push vymarkov/mongo:$TAG
